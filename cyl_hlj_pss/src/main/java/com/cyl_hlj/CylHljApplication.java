12c1ddfde06f7f78b822728b9ca8d492cb7e1d0a
your user name <your user email>
2019-01-25 21:04:59 +0800

    Initial commit

---
+++ cyl_hlj_pss/src/main/java/com/cyl_hlj/CylHljApplication.java
@@ -0,0 +1,14 @@
+package com.cyl_hlj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CylHljApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CylHljApplication.class, args);
+	}
+
+}
+
